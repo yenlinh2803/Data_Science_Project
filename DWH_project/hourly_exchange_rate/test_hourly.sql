7ebe40963c06be9913031604e2d146cc2f569ac5
linh.trinh <tnyenlinh@gmail.com>
2019-09-04 21:09:08 +0700

    commit

--- DWH_project/hourly_exchange_rate/test_hourly.sql
+++ DWH_project/hourly_exchange_rate/test_hourly.sql
@@ -9,8 +9,8 @@ AS $procedure$
        		v_loop_count				int4;
        		v_loop_hour					int4;
             --v_loop_time             varchar(150);
-            p_currency_id				smallint;
-       		
+            v_currency_id				smallint;
+       		v_currency_array			int[];
 			v_loop_time_timestamp		TIMESTAMP WITHOUT time zone;
            	v_t01						TIMESTAMP WITHOUT time zone;
            	v_t02						TIMESTAMP WITHOUT time zone;
@@ -28,13 +28,23 @@ AS $procedure$
 		BEGIN
 				v_t01:= CURRENT_TIMESTAMP;
 				v_t01:= v_t01::timestamp;
-			
+				--v_currency_array:= array[20,118];
 				
-				p_currency_id:= 20;
-			
+				
+				select array(SELECT distinct id FROM edw_dim.cur01_currency_t) into v_currency_array;
+				RAISE NOTICE 'array %',v_currency_array;
+				
+				
+				--FOR v_currency_id in v_currency_array 
+				--loop
+				--	RAISE NOTICE 'v_currency_id %',v_currency_id;
+				--end loop;
+				
+				
+				v_currency_id:= 02;
 				SELECT max(capturedate) INTO v_max_fx_hourly 
 				FROM dev.fx_rate_hourly 
-				where currency_id =  p_currency_id
+				where currency_id =  v_currency_id
 				group by currency_id ; 
 				v_max_fx_hourly:= '2018-01-01 00:00:00'::timestamp;
 			
@@ -53,11 +63,11 @@ AS $procedure$
    				RAISE NOTICE 'Ext max date %',v_ext_fx_dt;
    	
    	
-              	--if v_max_fx_hourly > (select least(v_cer_max_dt,v_mpd_max_dt,v_ext_fx_dt))
-              	--then
-              	--	RAISE NOTICE 'Time smallest  %',v_max_fx_hourly;
-              	--	RAISE NOTICE 'Batch inserted to hold table %';
-              	--end if;
+              	if v_max_fx_hourly > (select least(v_cer_max_dt,v_mpd_max_dt,v_ext_fx_dt))
+              	then
+              		RAISE NOTICE 'Time smallest  %',v_max_fx_hourly;
+              		--RAISE NOTICE 'Batch inserted to hold table %';
+              	end if;
               	
               	if v_max_fx_hourly <= (select least(v_cer_max_dt,v_mpd_max_dt,v_ext_fx_dt))
               	then
@@ -65,7 +75,7 @@ AS $procedure$
               	
               		
               		
-              	
+              		
 					loop
 						-- v_loop_time:= to_char(v_max_fx_hourly, 'YYYY-MM-DD')||  make_time(v_loop_hour,0,0);
 						
@@ -77,8 +87,8 @@ AS $procedure$
 						end if;
 										
 						insert into dev.fx_rate_hourly(currency_id,usdrate,capturedate,provider,created_date_id,capturehour_id,ctrl_last_update_ts)
-						select p_currency_id, --currency_id
-						edw_dim.uf_get_usd_rate_for_currency(v_loop_time_timestamp::timestamp,p_currency_id),--usdrate
+						select v_currency_id, --currency_id
+						edw_dim.uf_get_usd_rate_for_currency(v_loop_time_timestamp::timestamp,v_currency_id),--usdrate
 						v_loop_time_timestamp, --capturedate
 						'ext01_external_fx_rate_t', --provider
 						to_char(v_loop_time_timestamp, 'YYYYMMDD')::int4, --created_date_id
@@ -89,12 +99,12 @@ AS $procedure$
 						
 						v_loop_hour := v_loop_hour + 1;
 						v_max_fx_hourly:= v_loop_time_timestamp; 
-						if v_max_fx_hourly >= '2018-03-05 00:00:00' 
+						if v_max_fx_hourly >= '2018-01-02 00:00:00' 
 						then 
 							v_t02:=  CURRENT_TIMESTAMP;
 							v_t02:= v_t02::timestamp;
 				        	v_time_run:= EXTRACT(EPOCH FROM (v_t02 - v_t01));
-				        	RAISE NOTICE 'time run: %',v_time_run*100;
+				        	RAISE NOTICE 'time run: %',v_time_run;
 							return;
 									
 						end if;
@@ -107,6 +117,7 @@ AS $procedure$
 				
 								
 				end if;
+				
 				                
             END 
      $procedure$
