0c36fcab7e092fc5c2e6172e307dbaaa9c0f26da
yenlinh2803 <tnyenlinh@gmail.com>
2020-02-19 00:44:28 +0700

    commit

---
+++ Bodie_Challenge/research.py
@@ -0,0 +1,24 @@
+#Link chanllenge:
+#https://www.upwork.com/jobs/Data-Science-Challenge_~0163d39191020232c2
+
+"""
+This is a toy problem designed to test your skills in data science and data analysis.  You can take as much or as little time as you like to complete the exercise, although obviously you will want to showcase your skills.
+
+If you complete the challenge successfully we will invite you to work on some real projects in data science that involve developing machine learning and analytics frameworks in Python and R.
+
+Bodie, a small town in the Old West, is a dangerous place to live and everyone from fur trappers to cowboys and lumberjacks need firearms to survive. 
+Here, you have started a lucrative business as bullets maker six years ago (in distant 1810) named ‘Taylor & Co’.
+As time goes by, the volume of your company’s sales has reached a point where you need to forecast future sales to efficiently manage raw material demands.
+
+As some bullets are always sold together with firearms, your sales have different destination channels: 1) Together with Firearms (TF sales) and 2) Replacement Sales (RS), for bullets requested by local shops (to be sold separately). 
+While you have precise estimates from firearm makers regarding the TF market bullets, you only have a few variables that help you forecast future RS sales (along with historical sales data since 1810). 
+
+Your company supplies two brands of firearm makers: Remington and Henry Lever. Each company sells different handgun models which require custom-fitted bullets. 
+Your job is to forecast the sales of each bullet model that is going through the RS channel, as well as the sales aggregate by handgun (RS only).
+Together with historical sales data you are provided with firearm sales (time series) for both Remington and Henry Level models to help you with the task.
+
+We would like you to return a 12-months ahead forecast with a methodology of your liking as well as a brief document describing the thinking process of your journey through the prairies of the Old West.
+You can develop your code both using Python or R, and we ask you to attach the code ( script and/ or jupyter notebook and/ or github repo as well as all the related visualizations) to the csv you will provide as task solution.
+
+As a pioneer data scientist in the Old West you are well ahead of your times, but we are confident that you will use your mastery to estimate an accurate sales forecast.
+"""
