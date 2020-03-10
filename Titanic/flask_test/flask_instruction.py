124ff70d6a21c4bce0ada05663175a3b793401ac
Yen Linh Trinh <yenlinh.trinh@vn.woowahan.com>
2020-03-10 14:00:19 +0700

    commit create API for ML model

---
+++ Titanic/flask_test/flask_instruction.py
@@ -0,0 +1,16 @@
+call API Postman :
+POST: 
+localhost:12345/predict
+
+Body:
+[
+	{"Age":85, "Sex": "male", "Embarked":"S"},
+	{"Age":24, "Sex": "male", "Embarked":"C"},
+	{"Age":82, "Sex": "female", "Embarked":"C"}
+	]
+
+
+Result exmaple:
+{
+  "prediction": "[0, 0, 1]"
+}
